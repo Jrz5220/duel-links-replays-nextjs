16f71d0b76e2bccd3b526c0d7568051541c96549
Jrz5220 <felixlazo94@gmail.com>
2025-04-16 21:22:13 -0400

    Not sure is userSession.ts model is needed

--- app/models/userSession.ts
+++ app/models/userSession.ts
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
+// I dont think I ever use this model, but I will keep it for now
+// I think this is used for the session cookie, but I am not sure
+
 const userSessionSchema = new mongoose.Schema({
     id: String,
     expires: Date,
