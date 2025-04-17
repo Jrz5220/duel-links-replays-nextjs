f1b4cd06c23b37e1351b1edfe9915f023acb424c
Jrz5220 <felixlazo94@gmail.com>
2025-04-16 21:26:04 -0400

    Not sure if 500.js error hanlder page is needed

--- app/pages/500.js
+++ app/pages/500.js
@@ -1,3 +1,6 @@
+// I dont know if this is the correct path for the 500 error page
+// This could be a global error page if a directory does not contain an custom error.ts page
+
 export default function Custom500() {
     return <h1>pages/500.js - Server error occured!</h1>
 }
